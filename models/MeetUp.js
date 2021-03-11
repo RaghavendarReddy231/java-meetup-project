7b38e9509f8d3a4662443e96b23e94a98c9053b5
m_srinivas <cnu.prince8@gmail.com>
2021-03-11 14:26:17 +0530

    cred

--- models/MeetUp.js
+++ models/MeetUp.js
@@ -1,14 +1,14 @@
 const mongoose =  require('mongoose');
 const { Schema } = mongoose;
-
+const Meetup = require('../models/')
   //creating user schema
 
-  const Meetup = new Meetup({
-    name: String,
-    description: String,
-    meetingTime: { type : Date, default: Date.now },
-    Address: String
-  });
+  // const Meetup = new Meetup({
+  //   name: String,
+  //   description: String,
+  //   meetingTime: { type : Date, default: Date.now },
+  //   Address: String
+  // });
 
 const usersModel = mongoose.model('users', users);
 
